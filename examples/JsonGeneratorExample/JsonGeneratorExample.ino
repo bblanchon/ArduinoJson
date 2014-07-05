8baf21b6e81821f9bc008c2ab87ef2af35801046
Benoit Blanchon <github@benoitblanchon.fr>
2014-07-05 21:39:56 +0200

    Minor changes

--- examples/JsonGeneratorExample/JsonGeneratorExample.ino
+++ examples/JsonGeneratorExample/JsonGeneratorExample.ino
@@ -11,17 +11,16 @@ void setup()
 {
     Serial.begin(9600);
 
-    JsonHashTable<3> json;
-
-    json.add("sensor", "gps");
-    json.add("time", 1351824120);
-
-    JsonArray<3> array;
+    JsonArray<2> array;
     array.add(48.756080, 6);
     array.add(2.302038, 6);
-    json.add("data", array);
 
-    Serial.print(json); // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
+    JsonHashTable<3> root; 
+    root.add("sensor", "gps");
+    root.add("time", 1351824120);
+    root.add("data", array);
+
+    Serial.print(root); // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
 }
 
 void loop()
