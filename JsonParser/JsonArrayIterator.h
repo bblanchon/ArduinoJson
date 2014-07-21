d3d0da2d7f0afe62f243d782db31274c6ad97d36
Benoit Blanchon <github@benoitblanchon.fr>
2014-07-21 14:17:27 +0200

    Added comments

--- JsonParser/JsonArrayIterator.h
+++ JsonParser/JsonArrayIterator.h
@@ -12,26 +12,31 @@ namespace ArduinoJson
 {
     namespace Parser
     {
+        // An iterator for JsonArray.
         class JsonArrayIterator : JsonToken
         {
         public:
 
+            // Create an iterator pointing at the specified JsonToken.
             JsonArrayIterator(JsonToken token)
                 : JsonToken(token)
             {
 
             }
 
+            // Move iterator forward.
             void operator++()
             {
                 *this = JsonArrayIterator(nextSibling());
             }
 
+            // Get the value pointed by the iterator.
             JsonValue operator*() const
             {
                 return JsonValue(*this);
             }
 
+            // Test iterator equality.
             bool operator!= (const JsonArrayIterator& other) const
             {
                 return JsonToken::operator!=(other);
