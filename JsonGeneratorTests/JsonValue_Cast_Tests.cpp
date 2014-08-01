15d3068d78d6f44b7020f7ecd6227f0e129179d0
Benoit Blanchon <github@benoitblanchon.fr>
2014-08-01 14:47:48 +0200

    Test casting a JsonValue to a string

---
+++ JsonGeneratorTests/JsonValue_Cast_Tests.cpp
@@ -0,0 +1,36 @@
+/*
+* Arduino JSON library
+* Benoit Blanchon 2014 - MIT License
+*/
+
+#include "CppUnitTest.h"
+#include "StringBuilder.h"
+#include "JsonValue.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace ArduinoJson::Generator;
+using namespace ArduinoJson::Internals;
+
+namespace JsonGeneratorTests
+{
+    TEST_CLASS(JsonValue_Cast_Tests)
+    {
+        JsonValue value;
+
+    public:
+
+        TEST_METHOD(String)
+        {            
+            value = "hello";
+            mustCastTo("hello");
+        }
+
+    private:
+
+        void mustCastTo(const char* expected)
+        {
+            const char* actual = value;
+            Assert::AreEqual(expected, actual);
+        }
+    };
+}
