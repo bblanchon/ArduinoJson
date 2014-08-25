f29904e2179a7848279ec5fc4c21a33304fddf18
Benoit Blanchon <github@benoitblanchon.fr>
2014-08-25 09:06:21 +0200

    Added skeleton of class IndentedPrintDecorator

---
+++ JsonGenerator/IndentedPrintDecorator.cpp
@@ -0,0 +1,11 @@
+/*
+* Arduino JSON library
+* Benoit Blanchon 2014 - MIT License
+*/
+
+#include "IndentedPrintDecorator.h"
+
+size_t IndentedPrintDecorator::write(uint8_t c)
+{
+    return print.write(c);
+}
---
+++ JsonGenerator/IndentedPrintDecorator.h
@@ -0,0 +1,26 @@
+#pragma once
+/*
+* Arduino JSON library
+* Benoit Blanchon 2014 - MIT License
+*/
+
+#pragma once
+
+#include "Print.h"
+
+class IndentedPrintDecorator : public Print
+{
+public:
+
+    IndentedPrintDecorator(Print& p)
+        : currentLevel(0), print(p)
+    {
+    }
+
+    virtual size_t write(uint8_t);
+
+private:
+    int currentLevel;
+    Print& print;
+};
+
