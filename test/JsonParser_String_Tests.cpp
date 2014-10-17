1f6bd5c04d50147801af4371b8df6a516fc484ca
Benoit Blanchon <github@benoitblanchon.fr>
2014-10-17 22:32:55 +0200

    Added more test of escaped chars

--- test/JsonParser_String_Tests.cpp
+++ test/JsonParser_String_Tests.cpp
@@ -57,3 +57,51 @@ TEST_F(JsonParser_String_Tests, EscapedSingleQuote)
     whenInputIs("\"hello \\\'world\\\'\"");
     outputMustBe("hello 'world'");
 }
+
+TEST_F(JsonParser_String_Tests, EscapedSolidus)
+{
+    whenInputIs("\"hello \\/world\\/\"");
+    outputMustBe("hello /world/");
+}
+
+TEST_F(JsonParser_String_Tests, EscapedReverseSolidus)
+{
+    whenInputIs("\"hello \\\\world\\\\\"");
+    outputMustBe("hello \\world\\");
+}
+
+TEST_F(JsonParser_String_Tests, EscapedBackspace)
+{
+    whenInputIs("\"hello \\bworld\\b");
+    outputMustBe("hello \bworld\b");
+}
+
+TEST_F(JsonParser_String_Tests, EscapedFormfeed)
+{
+    whenInputIs("\"hello \\fworld\\f");
+    outputMustBe("hello \fworld\f");
+}
+
+TEST_F(JsonParser_String_Tests, EscapedNewline)
+{
+    whenInputIs("\"hello \\nworld\\n");
+    outputMustBe("hello \nworld\n");
+}
+
+TEST_F(JsonParser_String_Tests, EscapedCarriageReturn)
+{
+    whenInputIs("\"hello \\rworld\\r");
+    outputMustBe("hello \rworld\r");
+}
+
+TEST_F(JsonParser_String_Tests, EscapedTab)
+{
+    whenInputIs("\"hello \\tworld\\t");
+    outputMustBe("hello \tworld\t");
+}
+
+TEST_F(JsonParser_String_Tests, AllEscapedCharsTogether)
+{
+    whenInputIs("\"1\\\"2\\\\3\\/4\\b5\\f6\\n7\\r8\\t9\"");
+    outputMustBe("1\"2\\3/4\b5\f6\n7\r8\t9");
+}
