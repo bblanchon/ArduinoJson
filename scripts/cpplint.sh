d8e595907b708fe8b8bdc5e20a92b8c77d914745
Benoit Blanchon <github@benoitblanchon.fr>
2014-11-06 13:59:38 +0100

    Muted runtime/references

--- scripts/cpplint.sh
+++ scripts/cpplint.sh
@@ -1,5 +1,5 @@
 CPPLINT="python third-party/cpplint/cpplint.py"
-FLAGS="--filter=-runtime/printf,-runtime/int,-readability/todo,-build/namespace"
+FLAGS="--filter=-runtime/printf,-runtime/int,-readability/todo,-build/namespace,-runtime/references"
 
 cd ..
 $CPPLINT $FLAGS $(find include src test -regex ".*\.[hc]pp$")
