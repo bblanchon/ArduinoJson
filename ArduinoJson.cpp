41651136bfbd21f23da2cfe6f1442550a7dbde5e
Benoit Blanchon <github@benoitblanchon.fr>
2015-02-07 20:05:02 +0100

    Switched back to old library layout (issues #39, #43 and #45)

---
+++ ArduinoJson.cpp
@@ -0,0 +1,23 @@
+// Copyright Benoit Blanchon 2014-2015
+// MIT License
+//
+// Arduino JSON library
+// https://github.com/bblanchon/ArduinoJson
+
+#ifdef ARDUINO
+
+// This file is here to help the Arduino IDE find the other files.
+
+#include "src/Arduino/Print.cpp"
+#include "src/Internals/IndentedPrint.cpp"
+#include "src/Internals/JsonParser.cpp"
+#include "src/Internals/List.cpp"
+#include "src/Internals/Prettyfier.cpp"
+#include "src/Internals/QuotedString.cpp"
+#include "src/Internals/StringBuilder.cpp"
+#include "src/JsonArray.cpp"
+#include "src/JsonBuffer.cpp"
+#include "src/JsonObject.cpp"
+#include "src/JsonVariant.cpp"
+
+#endif
---
+++ ArduinoJson.h
@@ -0,0 +1,13 @@
+// Copyright Benoit Blanchon 2014-2015
+// MIT License
+//
+// Arduino JSON library
+// https://github.com/bblanchon/ArduinoJson
+
+#ifdef ARDUINO
+
+// This file is here to help the Arduino IDE find the other files.
+
+#include "include/ArduinoJson.h"
+
+#endif
