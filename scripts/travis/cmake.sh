987afff0e97a2b5311c946b9b4727b0791053f92
Benoit Blanchon <github@benoitblanchon.fr>
2016-02-17 10:15:40 +0100

    Travis: added GCC 4.4, 4.5, 4.7, 4.8, 4.9, 5.2 and Clang 3.5, 3.6, 3.7

--- scripts/travis/cmake.sh
+++ scripts/travis/cmake.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -eux
 
-export CC=clang
-export CXX=clang++
-
 curl -sS https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz | tar xz -C /tmp --strip 1
 
-/tmp/bin/cmake .
+/tmp/bin/cmake -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER .
 make 
 make test
--- scripts/travis/coverage.sh
+++ scripts/travis/coverage.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -eux
 
-export CC=gcc
-export CXX=g++
-
 curl https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz | tar xz -C /tmp --strip 1
 
 /tmp/bin/cmake -DCOVERAGE=true .
