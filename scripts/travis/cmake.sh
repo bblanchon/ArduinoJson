fb554071dcff57bcd494d4696d1829ed5e149ba8
Benoit Blanchon <github@benoitblanchon.fr>
2017-01-21 19:49:44 +0100

    Travis: Added -fsanitize to several builds

--- scripts/travis/cmake.sh
+++ scripts/travis/cmake.sh
@@ -1,9 +1,30 @@
-#!/bin/sh -eux
+#!/bin/sh -ex
 
-URL=https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz
-
+if [ $(uname) = 'Darwin' ]; then
+	URL=https://cmake.org/files/v3.4/cmake-3.4.3-Darwin-x86_64.tar.gz
+	CMAKE=/tmp/CMake.app/Contents/bin/cmake
+	CTEST=/tmp/CMake.app/Contents/bin/ctest
+else
+	URL=https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz
+	CMAKE=/tmp/bin/cmake
+	CTEST=/tmp/bin/ctest
+fi
 curl -sS $URL | tar xz -C /tmp --strip 1
 
-/tmp/bin/cmake -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER .
-make 
-make test
+if [ -n "$GCC" ]; then
+	export CC="gcc-$GCC"
+	export CXX="g++-$GCC"
+fi
+
+if [ -n "$CLANG" ]; then
+	export CC="clang-$CLANG"
+	export CXX="clang++-$CLANG"
+fi
+
+if [ -n "$SANITIZE" ]; then
+	export CXXFLAGS="-fsanitize=$SANITIZE"
+fi
+
+$CMAKE .
+$CMAKE --build .
+$CTEST -VV .
--- scripts/travis/sanitize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eux
-
-curl https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz | tar xz -C /tmp --strip 1
-
-/tmp/bin/cmake -DSANITIZE=true .
-cmake --build .
-ctest -VV .
