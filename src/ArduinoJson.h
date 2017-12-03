d10277df31fb519486d006c94b7a76666b990324
Benoit Blanchon <github@benoitblanchon.fr>
2017-12-03 13:49:51 +0100

    Added a clear error message when compiled as C (issue #629)

--- src/ArduinoJson.h
+++ src/ArduinoJson.h
@@ -4,6 +4,14 @@
 
 #pragma once
 
+#ifdef __cplusplus
+
 #include "ArduinoJson.hpp"
 
 using namespace ArduinoJson;
+
+#else
+
+#error ArduinoJson requires a C++ compiler, please change file extension to .cc or .cpp
+
+#endif
