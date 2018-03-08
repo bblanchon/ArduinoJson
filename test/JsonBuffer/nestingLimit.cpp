1e492356684f236fd0d7c479e293b375d09287ff
Benoit Blanchon <github@benoitblanchon.fr>
2018-03-08 10:06:34 +0100

    Reproduced issue #693

--- test/JsonBuffer/nestingLimit.cpp
+++ test/JsonBuffer/nestingLimit.cpp
@@ -5,6 +5,11 @@
 #include <ArduinoJson.h>
 #include <catch.hpp>
 
+bool tryParse(const char *json, uint8_t nestingLimit) {
+  DynamicJsonBuffer buffer;
+  return buffer.parse(json, nestingLimit).success();
+}
+
 bool tryParseArray(const char *json, uint8_t nestingLimit) {
   DynamicJsonBuffer buffer;
   return buffer.parseArray(json, nestingLimit).success();
@@ -16,33 +21,45 @@ bool tryParseObject(const char *json, uint8_t nestingLimit) {
 }
 
 TEST_CASE("JsonParser nestingLimit") {
-  SECTION("ParseArrayWithNestingLimit0") {
-    REQUIRE(true == tryParseArray("[]", 0));
-    REQUIRE(false == tryParseArray("[[]]", 0));
+  SECTION("parse()") {
+    SECTION("limit = 0") {
+      REQUIRE(true == tryParse("\"toto\"", 0));
+      REQUIRE(true == tryParse("[]", 0));
+      REQUIRE(false == tryParse("[\"toto\"]", 0));
+    }
   }
 
-  SECTION("ParseArrayWithNestingLimit1") {
-    REQUIRE(true == tryParseArray("[[]]", 1));
-    REQUIRE(false == tryParseArray("[[[]]]", 1));
+  SECTION("parseArray()") {
+    SECTION("limit = 0") {
+      REQUIRE(true == tryParseArray("[]", 0));
+      REQUIRE(false == tryParseArray("[[]]", 0));
+    }
+
+    SECTION("limit = 1") {
+      REQUIRE(true == tryParseArray("[[]]", 1));
+      REQUIRE(false == tryParseArray("[[[]]]", 1));
+    }
+
+    SECTION("limit = 2") {
+      REQUIRE(true == tryParseArray("[[[]]]", 2));
+      REQUIRE(false == tryParseArray("[[[[]]]]", 2));
+    }
   }
 
-  SECTION("ParseArrayWithNestingLimit2") {
-    REQUIRE(true == tryParseArray("[[[]]]", 2));
-    REQUIRE(false == tryParseArray("[[[[]]]]", 2));
-  }
+  SECTION("parseObject()") {
+    SECTION("limit = 0") {
+      REQUIRE(true == tryParseObject("{}", 0));
+      REQUIRE(false == tryParseObject("{\"key\":{}}", 0));
+    }
 
-  SECTION("ParseObjectWithNestingLimit0") {
-    REQUIRE(true == tryParseObject("{}", 0));
-    REQUIRE(false == tryParseObject("{\"key\":{}}", 0));
-  }
+    SECTION("limit = 1") {
+      REQUIRE(true == tryParseObject("{\"key\":{}}", 1));
+      REQUIRE(false == tryParseObject("{\"key\":{\"key\":{}}}", 1));
+    }
 
-  SECTION("ParseObjectWithNestingLimit1") {
-    REQUIRE(true == tryParseObject("{\"key\":{}}", 1));
-    REQUIRE(false == tryParseObject("{\"key\":{\"key\":{}}}", 1));
-  }
-
-  SECTION("ParseObjectWithNestingLimit2") {
-    REQUIRE(true == tryParseObject("{\"key\":{\"key\":{}}}", 2));
-    REQUIRE(false == tryParseObject("{\"key\":{\"key\":{\"key\":{}}}}", 2));
+    SECTION("limit = 2") {
+      REQUIRE(true == tryParseObject("{\"key\":{\"key\":{}}}", 2));
+      REQUIRE(false == tryParseObject("{\"key\":{\"key\":{\"key\":{}}}}", 2));
+    }
   }
 }
