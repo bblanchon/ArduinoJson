3808fe20b3daa7fe872d9ee21bcf23d07cf8dd39
Zhenyu Wu <Adam_5Wu@hotmail.com>
2018-03-08 19:10:13 -0500

    Adjust old test cases and add new test cases

--- test/JsonBuffer/nestingLimit.cpp
+++ test/JsonBuffer/nestingLimit.cpp
@@ -48,8 +48,10 @@ TEST_CASE("JsonParser nestingLimit") {
   SECTION("parse()") {
     SECTION("limit = 0") {
       SHOULD_WORK(jb.parse("\"toto\"", 0));
-      SHOULD_WORK(jb.parse("[]", 0));
-      SHOULD_WORK(jb.parse("{}", 0));
+      SHOULD_WORK(jb.parse("123", 0));
+      SHOULD_WORK(jb.parse("true", 0));
+      SHOULD_FAIL(jb.parse("[]", 0));
+      SHOULD_FAIL(jb.parse("{}", 0));
       SHOULD_FAIL(jb.parse("[\"toto\"]", 0));
       SHOULD_FAIL(jb.parse("{\"toto\":1}", 0));
     }
@@ -57,6 +59,8 @@ TEST_CASE("JsonParser nestingLimit") {
     SECTION("limit = 1") {
       SHOULD_WORK(jb.parse("[\"toto\"]", 1));
       SHOULD_WORK(jb.parse("{\"toto\":1}", 1));
+      SHOULD_FAIL(jb.parse("{\"toto\":{}}", 1));
+      SHOULD_FAIL(jb.parse("{\"toto\":[]}", 1));
       SHOULD_FAIL(jb.parse("[[\"toto\"]]", 1));
       SHOULD_FAIL(jb.parse("[{\"toto\":1}]", 1));
     }
