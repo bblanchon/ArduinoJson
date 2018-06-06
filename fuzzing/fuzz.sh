7b229e4c3810c65fc3c5620a42fc3f934d7f1b59
Benoit Blanchon <github@benoitblanchon.fr>
2018-06-06 11:19:36 +0200

    Added fuzzing to travis

--- fuzzing/fuzz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script mimics an invocation from https://github.com/google/oss-fuzz
-
-cd $(dirname $0)
-export CXX='clang++'
-export CXXFLAGS='-fsanitize-coverage=trace-pc-guard -fsanitize=address'
-export LIB_FUZZING_ENGINE=-lFuzzer
-make OUT=.
-./json_fuzzer my_corpus seed_corpus -max_len=1024 -timeout=10
