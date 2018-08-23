0577b894184ff1fd4db5ac86f24627b5c13fe7eb
baruch <baruch@ibn-labs.com>
2018-08-23 15:06:31 +0300

    fix include path

--- component.mk
+++ component.mk
@@ -1,6 +1,1 @@
-#IBN_GEN_LIB := $(patsubst $(COMPONENT_PATH)/%,%,$(sort $(dir $(wildcard $(COMPONENT_PATH)/libraries/*/*/))))
-
-COMPONENT_ADD_INCLUDEDIRS := .
-#COMPONENT_PRIV_INCLUDEDIRS := cores/esp32/libb64
-#COMPONENT_SRCDIRS := cores/esp32/libb64 cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
-#CXXFLAGS += -fno-rtti
+COMPONENT_ADD_INCLUDEDIRS := src
