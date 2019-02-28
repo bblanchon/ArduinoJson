27c08b785df58932b80d1b8818205c96406c9e1c
Benoit Blanchon <github@benoitblanchon.fr>
2019-02-28 17:26:29 +0100

    Travis: Assume CMake is installed

--- scripts/travis/cmake.sh
+++ scripts/travis/cmake.sh
@@ -1,16 +1,5 @@
 #!/bin/sh -ex
 
-if [ "$(uname)" = 'Darwin' ]; then
-	URL=https://cmake.org/files/v3.4/cmake-3.4.3-Darwin-x86_64.tar.gz
-	CMAKE=/tmp/CMake.app/Contents/bin/cmake
-	CTEST=/tmp/CMake.app/Contents/bin/ctest
-else
-	URL=https://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz
-	CMAKE=/tmp/bin/cmake
-	CTEST=/tmp/bin/ctest
-fi
-curl -sS $URL | tar xz -C /tmp --strip 1
-
 if [ -n "$GCC" ]; then
 	export CC="gcc-$GCC"
 	export CXX="g++-$GCC"
@@ -28,6 +17,6 @@ else
 	BUILD_TYPE="Release"
 fi
 
-$CMAKE -DCMAKE_BUILD_TYPE=$BUILD_TYPE .
-$CMAKE --build .
-$CTEST --output-on-failure .
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .
+cmake --build .
+ctest --output-on-failure .
--- scripts/travis/coverage.sh
+++ scripts/travis/coverage.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -eux
 
-curl https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz | tar xz -C /tmp --strip 1
-
-/tmp/bin/cmake -DCOVERAGE=true .
+cmake -DCOVERAGE=true .
 make 
 make test
 
