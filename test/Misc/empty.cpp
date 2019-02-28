b171872b58aa1299a718781359b128e5cf5bc404
Benoit Blanchon <github@benoitblanchon.fr>
2019-02-28 09:32:56 +0100

    Add an empty cpp file to detect warnings muted by catch.hpp

---
+++ test/Misc/empty.cpp
@@ -0,0 +1,7 @@
+// ArduinoJson - arduinojson.org
+// Copyright Benoit Blanchon 2014-2019
+// MIT License
+
+// catch.hpp mutes several warnings, this file allows to detect them
+
+#include "ArduinoJson.h"
