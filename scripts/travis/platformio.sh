81bb3fce97ab91225910c14a0b8de8a1a3947fd5
Benoit Blanchon <github@benoitblanchon.fr>
2019-04-20 15:45:38 +0200

    Fixed build on platformio

--- scripts/travis/platformio.sh
+++ scripts/travis/platformio.sh
@@ -4,6 +4,18 @@ pip install --user platformio
 
 rm -r test
 
+case $BOARD in
+uno)
+  platformio lib install 868 # SD library
+  platformio lib install 872 # Ethernet library
+  ;;
+esp01)
+  platformio lib uninstall 161 || true
+  platformio lib uninstall 868 || true
+  platformio lib uninstall 872 || true
+  ;;
+esac
+
 for EXAMPLE in $PWD/examples/*/*.ino; 
 do
 	platformio ci $EXAMPLE -l '.' -b $BOARD
