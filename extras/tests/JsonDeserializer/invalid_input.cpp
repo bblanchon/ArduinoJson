5ec062cc71ebeb59fb8090415d8d48552e54d7a0
Benoit Blanchon <git@benoitblanchon.fr>
2020-01-09 15:39:45 +0100

    Reduced Unicode conversion code size (-122 bytes on AVR)

--- extras/tests/JsonDeserializer/invalid_input.cpp
+++ extras/tests/JsonDeserializer/invalid_input.cpp
@@ -7,10 +7,9 @@
 #include <catch.hpp>
 
 TEST_CASE("Invalid JSON input") {
-  const char* testCases[] = {
-      "'\\u'",     "'\\u000g'",  "'\\u000'",  "'\\u000G'", "'\\ud83d\\ud83d'",
-      "'\\udda4'", "'\\ud83d_'", "'\\u000/'", "\\x1234",   "6a9",
-      "1,",        "2]",         "3}"};
+  const char* testCases[] = {"'\\u'",     "'\\u000g'", "'\\u000'", "'\\u000G'",
+                             "'\\u000/'", "\\x1234",   "6a9",      "1,",
+                             "2]",        "3}"};
   const size_t testCount = sizeof(testCases) / sizeof(testCases[0]);
 
   DynamicJsonDocument doc(4096);
@@ -23,7 +22,14 @@ TEST_CASE("Invalid JSON input") {
 }
 
 TEST_CASE("Invalid JSON input that should pass") {
-  const char* testCases[] = {"nulL", "tru3", "fals3"};
+  const char* testCases[] = {
+      "nulL",
+      "tru3",
+      "fals3",
+      "'\\ud83d'",         // leading surrogate without a trailing surrogate
+      "'\\udda4'",         // trailing surrogate without a leading surrogate
+      "'\\ud83d\\ud83d'",  // two leading surrogates
+  };
   const size_t testCount = sizeof(testCases) / sizeof(testCases[0]);
 
   DynamicJsonDocument doc(4096);
--- src/ArduinoJson/Json/JsonDeserializer.hpp
+++ src/ArduinoJson/Json/JsonDeserializer.hpp
@@ -189,7 +189,9 @@ class JsonDeserializer {
 
   DeserializationError parseQuotedString(const char *&result) {
     StringBuilder builder = _stringStorage.startString();
+#if ARDUINOJSON_DECODE_UNICODE
     uint16_t surrogate1 = 0;
+#endif
     const char stopChar = current();
 
     move();
@@ -205,23 +207,21 @@ class JsonDeserializer {
         if (c == '\0') return DeserializationError::IncompleteInput;
         if (c == 'u') {
 #if ARDUINOJSON_DECODE_UNICODE
-          uint16_t codepoint;
           move();
-          DeserializationError err = parseCodepoint(codepoint);
+          uint32_t codepoint;
+          uint16_t codeunit;
+          DeserializationError err = parseHex4(codeunit);
           if (err) return err;
-          if (codepoint >= 0xd800 && codepoint <= 0xdbff) {
-            if (surrogate1 > 0) return DeserializationError::InvalidInput;
-            surrogate1 = codepoint;
-          } else if (codepoint >= 0xdc00 && codepoint <= 0xdfff) {
-            if (surrogate1 == 0) return DeserializationError::InvalidInput;
-            uint32_t codepoint32 = 0x10000;
-            codepoint32 += static_cast<uint32_t>(surrogate1 - 0xd800) << 10;
-            codepoint32 += codepoint - 0xdc00;
-            Utf8::encodeCodepoint(codepoint32, builder);
-            surrogate1 = 0;
+          if (codeunit >= 0xDC00) {
+            codepoint =
+                uint32_t(0x10000 | ((surrogate1 << 10) | (codeunit & 0x3FF)));
+          } else if (codeunit < 0xd800) {
+            codepoint = codeunit;
           } else {
-            Utf8::encodeCodepoint(codepoint, builder);
+            surrogate1 = codeunit & 0x3FF;
+            continue;
           }
+          Utf8::encodeCodepoint(codepoint, builder);
           continue;
 #else
           return DeserializationError::NotSupported;
@@ -232,8 +232,6 @@ class JsonDeserializer {
         if (c == '\0') return DeserializationError::InvalidInput;
         move();
       }
-
-      if (surrogate1 > 0) return DeserializationError::InvalidInput;
 
       builder.append(c);
     }
@@ -312,14 +310,14 @@ class JsonDeserializer {
     return DeserializationError::InvalidInput;
   }
 
-  DeserializationError parseCodepoint(uint16_t &codepoint) {
-    codepoint = 0;
+  DeserializationError parseHex4(uint16_t &result) {
+    result = 0;
     for (uint8_t i = 0; i < 4; ++i) {
       char digit = current();
       if (!digit) return DeserializationError::IncompleteInput;
       uint8_t value = decodeHex(digit);
       if (value > 0x0F) return DeserializationError::InvalidInput;
-      codepoint = uint16_t((codepoint << 4) | value);
+      result = uint16_t((result << 4) | value);
       move();
     }
     return DeserializationError::Ok;
