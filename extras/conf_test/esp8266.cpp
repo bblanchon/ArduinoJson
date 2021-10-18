9e9bb30a57378b0edafdc9b45ac6391b8b2b0984
Benoit Blanchon <git@benoitblanchon.fr>
2021-10-18 16:43:47 +0200

    CI: Test ArduinoJson's configuration

---
+++ extras/conf_test/esp8266.cpp
@@ -0,0 +1,16 @@
+#include <ArduinoJson.h>
+
+static_assert(ARDUINOJSON_USE_LONG_LONG == 0, "ARDUINOJSON_USE_LONG_LONG");
+
+static_assert(ARDUINOJSON_SLOT_OFFSET_SIZE == 2,
+              "ARDUINOJSON_SLOT_OFFSET_SIZE");
+
+static_assert(ARDUINOJSON_LITTLE_ENDIAN == 1, "ARDUINOJSON_LITTLE_ENDIAN");
+
+static_assert(ARDUINOJSON_USE_DOUBLE == 0, "ARDUINOJSON_USE_DOUBLE");
+
+static_assert(sizeof(ARDUINOJSON_NAMESPACE::VariantSlot) == 16,
+              "sizeof(VariantSlot)");
+
+void setup() {}
+void loop() {}
---
+++ extras/conf_test/x64.cpp
@@ -0,0 +1,15 @@
+#include <ArduinoJson.h>
+
+static_assert(ARDUINOJSON_USE_LONG_LONG == 1, "ARDUINOJSON_USE_LONG_LONG");
+
+static_assert(ARDUINOJSON_SLOT_OFFSET_SIZE == 4,
+              "ARDUINOJSON_SLOT_OFFSET_SIZE");
+
+static_assert(ARDUINOJSON_LITTLE_ENDIAN == 1, "ARDUINOJSON_LITTLE_ENDIAN");
+
+static_assert(ARDUINOJSON_USE_DOUBLE == 1, "ARDUINOJSON_USE_DOUBLE");
+
+static_assert(sizeof(ARDUINOJSON_NAMESPACE::VariantSlot) == 32,
+              "sizeof(VariantSlot)");
+
+int main() {}
