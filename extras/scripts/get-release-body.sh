fcae33d57422fb206f3602e05415bdb7e2844454
Benoit Blanchon <git@benoitblanchon.fr>
2022-01-08 17:59:07 +0100

    Publish script: fix change log extraction code

--- extras/scripts/get-release-body.sh
+++ extras/scripts/get-release-body.sh
@@ -8,7 +8,7 @@ CHANGELOG="$2"
 cat << END
 ## Changes
 
-$(awk '/\* /{ FOUND=1; print; next } { if (FOUND) exit}' "$CHANGELOG")
+$(awk '/\* /{ FOUND=1 } /^[[:space:]]*$/ { if(FOUND) exit } { if(FOUND) print }' "$CHANGELOG")
 
 [View version history](https://github.com/bblanchon/ArduinoJson/blob/$TAG/CHANGELOG.md)
 END
