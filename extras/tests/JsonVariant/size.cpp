5577d18377452ed84c49f9df865d1ee663bb84da
Benoit Blanchon <git@benoitblanchon.fr>
2022-04-27 15:00:27 +0200

    JsonVariant: add tests for `size()`

---
+++ extras/tests/JsonVariant/size.cpp
@@ -0,0 +1,44 @@
+// ArduinoJson - https://arduinojson.org
+// Copyright © 2014-2022, Benoit BLANCHON
+// MIT License
+
+#include <ArduinoJson.h>
+#include <catch.hpp>
+
+TEST_CASE("JsonVariant::size()") {
+  DynamicJsonDocument doc(4096);
+  JsonVariant variant = doc.to<JsonVariant>();
+
+  SECTION("unbound reference") {
+    JsonVariant unbound;
+
+    CHECK(unbound.size() == 0);
+  }
+
+  SECTION("int") {
+    variant.set(42);
+
+    CHECK(variant.size() == 0);
+  }
+
+  SECTION("string") {
+    variant.set("hello");
+
+    CHECK(variant.size() == 0);
+  }
+
+  SECTION("object") {
+    variant["a"] = 1;
+    variant["b"] = 2;
+
+    CHECK(variant.size() == 2);
+  }
+
+  SECTION("linked object") {
+    StaticJsonDocument<1024> doc2;
+    doc2["hello"] = "world";
+    variant.link(doc2);
+
+    CHECK(variant.size() == 1);
+  }
+}
