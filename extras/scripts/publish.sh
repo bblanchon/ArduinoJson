ae0499fd5761c90eb7a56ea6a7f2efce1d3edd2d
Benoit Blanchon <git@benoitblanchon.fr>
2023-02-08 16:46:15 +0100

    Update `idf_component.yml`

--- extras/scripts/publish.sh
+++ extras/scripts/publish.sh
@@ -51,7 +51,7 @@ update_version_in_source () {
 }
 
 commit_new_version () {
-	git add src/ArduinoJson/version.hpp README.md CHANGELOG.md library.json library.properties appveyor.yml CMakeLists.txt
+	git add src/ArduinoJson/version.hpp README.md CHANGELOG.md library.json library.properties appveyor.yml CMakeLists.txt idf_component.yml
 	git commit -m "Set version to $VERSION"
 }
 
