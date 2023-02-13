0dd4a68913c8b6aa6e43342015f966143802ff22
Benoit Blanchon <git@benoitblanchon.fr>
2023-02-13 18:55:26 +0100

    Use only letters to encode configuration in the namespace

--- src/ArduinoJson/Namespace.hpp
+++ src/ArduinoJson/Namespace.hpp
@@ -11,15 +11,14 @@
 #ifndef ARDUINOJSON_VERSION_NAMESPACE
 
 #  define ARDUINOJSON_VERSION_NAMESPACE                                       \
-    ARDUINOJSON_CONCAT4(                                                      \
+    ARDUINOJSON_CONCAT3(                                                      \
         ARDUINOJSON_CONCAT4(V, ARDUINOJSON_VERSION_MAJOR,                     \
                             ARDUINOJSON_VERSION_MINOR,                        \
                             ARDUINOJSON_VERSION_REVISION),                    \
-        _,                                                                    \
-        ARDUINOJSON_HEX_DIGIT(                                                \
+        ARDUINOJSON_BIN2ALPHA(                                                \
             ARDUINOJSON_ENABLE_PROGMEM, ARDUINOJSON_USE_LONG_LONG,            \
             ARDUINOJSON_USE_DOUBLE, ARDUINOJSON_ENABLE_STRING_DEDUPLICATION), \
-        ARDUINOJSON_HEX_DIGIT(                                                \
+        ARDUINOJSON_BIN2ALPHA(                                                \
             ARDUINOJSON_ENABLE_NAN, ARDUINOJSON_ENABLE_INFINITY,              \
             ARDUINOJSON_ENABLE_COMMENTS, ARDUINOJSON_DECODE_UNICODE))
 
