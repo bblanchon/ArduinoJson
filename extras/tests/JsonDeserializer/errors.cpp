8fb187baedc1861a15fda06268b76209933df561
Benoit Blanchon <git@benoitblanchon.fr>
2023-07-27 14:52:58 +0200

    Tests: gather `deserializeJson()` errors test cases

---
+++ extras/tests/JsonDeserializer/errors.cpp
@@ -0,0 +1,103 @@
+// ArduinoJson - https://arduinojson.org
+// Copyright © 2014-2023, Benoit BLANCHON
+// MIT License
+
+#define ARDUINOJSON_DECODE_UNICODE 1
+#include <ArduinoJson.h>
+#include <catch.hpp>
+
+TEST_CASE("deserializeJson() returns IncompleteInput") {
+  const char* testCases[] = {
+      // strings
+      "\"\\",
+      "\"hello",
+      "\'hello",
+      // unicode
+      "'\\u",
+      "'\\u00",
+      "'\\u000",
+      // false
+      "f",
+      "fa",
+      "fal",
+      "fals",
+      // true
+      "t",
+      "tr",
+      "tru",
+      // null
+      "n",
+      "nu",
+      "nul",
+      // object
+      "{",
+      "{a",
+      "{a:",
+      "{a:1",
+      "{a:1,",
+      "{a:1,b",
+      "{a:1,b:",
+  };
+
+  for (auto input : testCases) {
+    SECTION(input) {
+      JsonDocument doc;
+      REQUIRE(deserializeJson(doc, input) ==
+              DeserializationError::IncompleteInput);
+    }
+  }
+}
+
+TEST_CASE("deserializeJson() returns InvalidInput") {
+  const char* testCases[] = {
+      // unicode
+      "'\\u'", "'\\u000g'", "'\\u000'", "'\\u000G'", "'\\u000/'", "\\x1234",
+      // numbers
+      "6a9", "1,", "2]", "3}",
+      // constants
+      "nulL", "tru3", "fals3",
+      // garbage
+      "%*$£¤"};
+
+  for (auto input : testCases) {
+    SECTION(input) {
+      JsonDocument doc;
+      REQUIRE(deserializeJson(doc, input) ==
+              DeserializationError::InvalidInput);
+    }
+  }
+}
+
+TEST_CASE("deserializeJson() oversees some edge cases") {
+  const char* testCases[] = {
+      "'\\ud83d'",         // leading surrogate without a trailing surrogate
+      "'\\udda4'",         // trailing surrogate without a leading surrogate
+      "'\\ud83d\\ud83d'",  // two leading surrogates
+  };
+
+  for (auto input : testCases) {
+    SECTION(input) {
+      JsonDocument doc;
+      REQUIRE(deserializeJson(doc, input) == DeserializationError::Ok);
+    }
+  }
+}
+
+TEST_CASE("deserializeJson() returns EmptyInput") {
+  JsonDocument doc;
+
+  SECTION("null") {
+    auto err = deserializeJson(doc, static_cast<const char*>(0));
+    REQUIRE(err == DeserializationError::EmptyInput);
+  }
+
+  SECTION("Empty string") {
+    auto err = deserializeJson(doc, "");
+    REQUIRE(err == DeserializationError::EmptyInput);
+  }
+
+  SECTION("Only spaces") {
+    auto err = deserializeJson(doc, "  \t\n\r");
+    REQUIRE(err == DeserializationError::EmptyInput);
+  }
+}
--- extras/tests/JsonDeserializer/misc.cpp
+++ extras/tests/JsonDeserializer/misc.cpp
@@ -7,69 +7,30 @@
 
 #include "Allocators.hpp"
 
-using namespace Catch::Matchers;
-
-using ArduinoJson::detail::sizeofObject;
-
-TEST_CASE("deserializeJson(JsonDocument&)") {
+TEST_CASE("deserializeJson() misc cases") {
   SpyingAllocator spy;
   JsonDocument doc(&spy);
 
-  SECTION("Edge cases") {
-    SECTION("null char*") {
-      DeserializationError err = deserializeJson(doc, static_cast<char*>(0));
-
-      REQUIRE(err != DeserializationError::Ok);
-    }
-
-    SECTION("null const char*") {
-      DeserializationError err =
-          deserializeJson(doc, static_cast<const char*>(0));
-
-      REQUIRE(err != DeserializationError::Ok);
-    }
-
-    SECTION("Empty input") {
-      DeserializationError err = deserializeJson(doc, "");
-
-      REQUIRE(err == DeserializationError::EmptyInput);
-    }
-
-    SECTION("Only spaces") {
-      DeserializationError err = deserializeJson(doc, "  \t\n\r");
-
-      REQUIRE(err == DeserializationError::EmptyInput);
-    }
-
-    SECTION("issue #628") {
-      DeserializationError err = deserializeJson(doc, "null");
-      REQUIRE(err == DeserializationError::Ok);
-      REQUIRE(doc.is<float>() == false);
-    }
-
-    SECTION("Garbage") {
-      DeserializationError err = deserializeJson(doc, "%*$£¤");
-
-      REQUIRE(err == DeserializationError::InvalidInput);
-    }
+  SECTION("null") {
+    DeserializationError err = deserializeJson(doc, "null");
+    REQUIRE(err == DeserializationError::Ok);
+    REQUIRE(doc.is<float>() == false);
   }
 
-  SECTION("Booleans") {
-    SECTION("True") {
-      DeserializationError err = deserializeJson(doc, "true");
+  SECTION("true") {
+    DeserializationError err = deserializeJson(doc, "true");
 
-      REQUIRE(err == DeserializationError::Ok);
-      REQUIRE(doc.is<bool>());
-      REQUIRE(doc.as<bool>() == true);
-    }
+    REQUIRE(err == DeserializationError::Ok);
+    REQUIRE(doc.is<bool>());
+    REQUIRE(doc.as<bool>() == true);
+  }
 
-    SECTION("False") {
-      DeserializationError err = deserializeJson(doc, "false");
+  SECTION("false") {
+    DeserializationError err = deserializeJson(doc, "false");
 
-      REQUIRE(err == DeserializationError::Ok);
-      REQUIRE(doc.is<bool>());
-      REQUIRE(doc.as<bool>() == false);
-    }
+    REQUIRE(err == DeserializationError::Ok);
+    REQUIRE(doc.is<bool>());
+    REQUIRE(doc.as<bool>() == false);
   }
 
   SECTION("Should clear the JsonVariant") {
