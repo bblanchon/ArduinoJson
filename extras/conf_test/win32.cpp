83516e1740fb3a97419a099b9af1c8a3269c8d7c
Benoit Blanchon <git@benoitblanchon.fr>
2024-08-23 15:24:45 +0200

    Split conf_test between linux and windows

---
+++ extras/conf_test/win32.cpp
@@ -0,0 +1,14 @@
+#include <ArduinoJson.h>
+
+static_assert(ARDUINOJSON_USE_LONG_LONG == 1, "ARDUINOJSON_USE_LONG_LONG");
+
+static_assert(ARDUINOJSON_SLOT_ID_SIZE == 2, "ARDUINOJSON_SLOT_ID_SIZE");
+
+static_assert(ARDUINOJSON_LITTLE_ENDIAN == 1, "ARDUINOJSON_LITTLE_ENDIAN");
+
+static_assert(ARDUINOJSON_USE_DOUBLE == 1, "ARDUINOJSON_USE_DOUBLE");
+
+static_assert(sizeof(ArduinoJson::detail::VariantSlot) == 16,
+              "sizeof(VariantSlot)");
+
+int main() {}
